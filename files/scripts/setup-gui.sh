c43926d36fa3835e8f5c80ec52f98efe3457469f
Assembly <81379378+tekq@users.noreply.github.com>
2025-12-27 20:51:18 +0200

    Add NVIDIA driver options to setup-gui.sh

--- files/scripts/setup-gui.sh
+++ files/scripts/setup-gui.sh
@@ -4,3 +4,9 @@ set -oue pipefail
 
 systemctl set-default graphical.target
 systemctl enable gdm.service
+
+rpm-ostree kargs \
+--append="quiet" \
+--append="splash" \
+--append="rd.driver.pre=nvidia" \
+--append="nvidia-drm.modeset=1"
