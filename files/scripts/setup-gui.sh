6fa2ad00e9cdb2c6e48a171f91e446a0b74d0792
Assembly <81379378+tekq@users.noreply.github.com>
2025-12-27 22:38:49 +0200

    Configure Malcontent Control and set permissions
    Add configuration for Malcontent Control desktop entry and restrict permissions.

--- files/scripts/setup-gui.sh
+++ files/scripts/setup-gui.sh
@@ -4,3 +4,6 @@ set -oue pipefail
 
 systemctl set-default graphical.target
 systemctl enable gdm.service
+
+echo "NoDisplay=true" >> /usr/share/applications/org.gnome.MalcontentControl.desktop
+chmod 000 /usr/bin/malcontent-control || true
